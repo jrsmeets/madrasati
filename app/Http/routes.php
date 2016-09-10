d4829e08b60c0e836df3abbca6934e1c43c76367
jrsmeets@xs4all.nl-github-20160904 <jrsmeets@xs4all.nl>
2016-09-10 17:49:21 +0200

    Basic setup.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/home', 'HomeController@index');
+    });
+});
